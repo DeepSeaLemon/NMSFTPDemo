60c6b6bf960754c067bddbbb4c9241dd1293000c
caoyuming <caoyuming@do-global.com>
2019-10-09 17:47:39 +0800

    第一次提交

---
+++ NewTest/Define.h
@@ -0,0 +1,20 @@
+//
+//  Define.h
+//  NewTest
+//
+//  Created by 10361 on 2019/10/9.
+//  Copyright © 2019 10361. All rights reserved.
+//
+
+#ifndef Define_h
+#define Define_h
+
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight  [UIScreen mainScreen].bounds.size.height
+
+#define rootPath @"/"
+
+#define testFilePath @"/Users/a10361/Downloads/Test.txt"
+#define testPath @"/Users/a10361/Downloads/"
+
+#endif /* Define_h */
---
+++ NewTest/Input/ViewController.m
@@ -0,0 +1,162 @@
+//
+//  ViewController.m
+//  NewTest
+//
+//  Created by 10361 on 2019/9/29.
+//  Copyright © 2019 10361. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "InputView.h"
+#import "InputModel.h"
+#import "FileListViewController.h"
+#import "LocalFilesListViewController.h"
+#import "FileListModel.h"
+
+@interface ViewController ()<InputViewBtnClickDelegate>
+
+@property (nonatomic,strong) InputView *inputView;
+
+@property (nonatomic,strong) InputModel *model;
+
+@property (nonatomic,strong) NMSFTP *sftp;
+
+@property (nonatomic,assign) BOOL sftpIsConnected;
+@property (nonatomic,strong) MBProgressHUD *hud;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"link";
+    self.sftpIsConnected = NO;
+    self.model = [[InputModel alloc]init];
+    self.inputView = [[InputView alloc]initWithFrame:self.view.frame];
+    self.inputView.delegate = self;
+    self.inputView.model = self.model;
+    [self.view addSubview:self.inputView];
+}
+
+- (void)clickInputViewBtn:(InputViewBtnType)senderType {
+    switch (senderType) {
+        case InputViewBtnTypeLink:
+            // 连接到服务器
+            [self linkToSFTP];
+            break;
+        case InputViewBtnTypeUp:
+            // 上传文件到服务器
+            [self upToSFTP];
+            break;
+        case InputViewBtnTypeDown:
+            // 下载文件到本地
+            [self downFromSFTP];
+            break;
+        case InputViewBtnTypeLook:
+            // 下载文件到本地
+            [self lookLocalFiles];
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark - click funcs
+
+- (void)linkToSFTP {
+    if (self.sftpIsConnected) {
+        [self.sftp disconnect];
+        self.sftpIsConnected = NO;
+        NSLog(@"SFTP disconnect");
+    }else {
+        NSString *urlStr = [NSString stringWithFormat:@"%@:%@",self.model.urlStr,self.model.portStr];
+        NMSSHSession *session = [NMSSHSession connectToHost:urlStr
+                                               withUsername:self.model.account];
+        
+        if (session.isConnected) {
+            NSLog(@"Successfully created a new session");
+        }
+        
+        [session authenticateByPassword:self.model.password];
+        
+        if (session.isAuthorized) {
+            NSLog(@"Successfully authorized");
+        }
+        
+        self.sftp = [[NMSFTP alloc]initWithSession:session];
+        
+        if ([self.sftp connect]) {
+            NSLog(@"SFTP connect success");
+            self.sftpIsConnected = YES;
+            [self pushToFileList];
+        }else {
+            NSLog(@"SFTP connect failure");
+            self.sftpIsConnected = NO;
+        }
+    }
+}
+
+- (void)pushToFileList {
+    FileListModel *model = [[FileListModel alloc]init];
+    model.fileListArray = [[self.sftp contentsOfDirectoryAtPath:rootPath] mutableCopy];
+    model.sftp = self.sftp;
+    model.path = rootPath;
+    
+    FileListViewController *fvc = [[FileListViewController alloc]init];
+    fvc.model = model;
+    [self.navigationController pushViewController:fvc animated:YES];
+}
+
+- (void)upToSFTP {
+    NSString *pathBundle = [[NSBundle mainBundle]pathForResource:@"Test" ofType:@"txt"];
+    NSData *data = [[NSData alloc]initWithContentsOfFile:pathBundle];
+    if ([self.sftp writeContents:data toFileAtPath:testFilePath]) {
+        NSLog(@"SFTP updata success");
+        NSLog(@"%@",[self.sftp contentsOfDirectoryAtPath:testPath]);
+    }else {
+        NSLog(@"SFTP updata failure");
+    }
+}
+
+- (void)downFromSFTP {
+    
+    if (self.sftp.isConnected) {
+        NSData *data = [self.sftp contentsAtPath:testFilePath];
+        [[LocalFilesManager shared]createFileWith:data fileName:@"Test.txt" result:^(BOOL isSuccess) {
+            if (isSuccess) {
+                self.hud.label.text = @"下载成功";
+                [self.hud showAnimated:YES];
+                [self.hud hideAnimated:YES afterDelay:1.5];
+            }else {
+                self.hud.label.text = @"下载失败";
+                [self.hud showAnimated:YES];
+                [self.hud hideAnimated:YES afterDelay:1.5];
+            }
+        }];
+    }
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+- (void)lookLocalFiles {
+    LocalFilesListViewController *lvc = [[LocalFilesListViewController alloc]init];
+    [self.navigationController pushViewController:lvc animated:YES];
+}
+
+- (void)dealloc {
+    [self.sftp disconnect];
+    NSLog(@"SFTP disconnect");
+}
+
+- (MBProgressHUD *)hud {
+    if (_hud) {
+        _hud = [[MBProgressHUD alloc]initWithView:self.view];
+        _hud.label.text = @"下载中...";
+        _hud.mode = MBProgressHUDModeText;
+    }
+    return _hud;
+}
+@end
